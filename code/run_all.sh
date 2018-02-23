0689d8df50bb07b58a142005267e3557c414522d
Connor Gilroy <ccgilroy@gmail.com>
2018-02-23 13:11:04 -0800

    fix Rscript for variable metadata

--- code/run_all.sh
+++ code/run_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # create lists of variable metadata
-Rscript -e "rmarkdown::render('code/data_processing/variable_metadata.Rmd', output_file='doc/vignettes/variable_metadata.html") &&
+Rscript -e "rmarkdown::render('code/data_processing/variable_metadata.Rmd', output_file='variable_metadata.html')" &&
+mv code/data_processing/variable_metadata.html doc/vignettes/variable_metadata.html &&
 
 # set up and do imputations
 # Rscript code/imputation/setup_mi_data.R &&
+echo "Done"
