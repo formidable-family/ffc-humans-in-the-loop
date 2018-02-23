3a4ef93e28dfea61f3eea863dbeb810092dfc055
Connor Gilroy <ccgilroy@gmail.com>
2018-02-23 12:08:10 -0800

    fix init.R file paths, add data.txt

--- code/run_all.sh
+++ code/run_all.sh
@@ -3,4 +3,4 @@
 Rscript -e "rmarkdown::render('code/data_processing/variable_metadata.Rmd', output_file='doc/vignettes/variable_metadata.html") &&
 
 # set up and do imputations
-Rscript code/imputation/setup_mi_data.R &&
+# Rscript code/imputation/setup_mi_data.R &&
