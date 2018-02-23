0306dd31ab84702d59b79dd0966b19df59ad40ce
Connor Gilroy <ccgilroy@gmail.com>
2018-02-23 12:02:26 -0800

    update variable metadata file paths

--- code/run_all.sh
+++ code/run_all.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-Rscript -e "rmarkdown::render('code/data_processing/variable_metadata.Rmd', output_file='doc/vignettes/variable_metadata.html"
+# create lists of variable metadata
+Rscript -e "rmarkdown::render('code/data_processing/variable_metadata.Rmd', output_file='doc/vignettes/variable_metadata.html") &&
+
+# set up and do imputations
+Rscript code/imputation/setup_mi_data.R &&
