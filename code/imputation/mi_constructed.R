b546bed6ad191c35762fd99ced893603e8ecb1a4
Connor Gilroy <ccgilroy@gmail.com>
2018-02-26 10:00:23 -0800

    break out multiple imputation of constructed variables into separate file because it keeps failing

--- code/imputation/mi_constructed.R
+++ code/imputation/mi_constructed.R
@@ -15,19 +15,21 @@ if (.Platform$OS.type == "windows") {
 
 source("code/data_processing/R/get_vars.R")
 
-background_ffvars <- readRDS("data/imputed/mi_setup/background_ffvars_to_mi.rds")
+# constructed ----
 
-categorical <- get_vars_categorical(background_ffvars)
+background_constructed <- readRDS("data/imputed/mi_setup/background_constructed_to_mi.rds")
+
+categorical <- get_vars_categorical(background_constructed)
 
 Sys.time()
-mi_background_ffvars <- 
-  amelia(background_ffvars, m = 5, 
+mi_background_constructed <- 
+  amelia(background_constructed, m = 5, 
          idvars = c("challengeID"),
          noms = categorical, 
-         empri = .1 * nrow(background_ffvars), 
+         empri = .1 * nrow(background_constructed), 
          tolerance = .005,
          boot.type = "none", 
          ncpus = 5)
 
-saveRDS(mi_background_ffvars, "data/imputed/background_ffvars_amelia.rds")
+saveRDS(mi_background_constructed, "data/imputed/background_constructed_amelia.rds")
 Sys.time()
