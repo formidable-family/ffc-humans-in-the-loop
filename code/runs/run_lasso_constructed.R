f1f8349f3d6d1055657500d7f1752cbf5b39324a
Connor Gilroy <ccgilroy@gmail.com>
2018-02-25 22:45:35 -0800

    update run_lasso_constructed file paths

--- code/runs/run_lasso_constructed.R
+++ code/runs/run_lasso_constructed.R
@@ -1,5 +1,10 @@
-library(tidyverse)
+set.seed(123)
+
+library(dplyr)
+library(purrr)
+library(readr)
 library(doParallel)
+library(parallel)
 registerDoParallel(cores = parallel::detectCores(logical = FALSE))
 
 # for more info on
@@ -7,23 +12,23 @@ registerDoParallel(cores = parallel::detectCores(logical = FALSE))
 # https://stackoverflow.com/a/21710769
 # https://stackoverflow.com/a/29001039
 
-source("models/calculate_penalty_factors.R")
-source("models/lasso.R")
-source("models/setup_lasso_all.R")
+source("code/models/calculate_penalty_factors.R")
+source("code/models/lasso.R")
+source("code/models/setup_lasso_all.R")
 
-source("utils/validate_imputed_background.R")
-source("utils/zip_prediction.R")
+source("code/utils/validate_imputed_background.R")
+source("code/utils/zip_prediction.R")
 
-source("https://raw.githubusercontent.com/ccgilroy/ffc-data-processing/master/R/get_vars.R")
-source("https://raw.githubusercontent.com/ccgilroy/ffc-data-processing/master/R/merge_train.R")
-source("https://raw.githubusercontent.com/ccgilroy/ffc-data-processing/master/R/subset_vars.R")
+source("code/data_processing/R/get_vars.R")
+source("code/data_processing/R/merge_train.R")
+source("code/data_processing/R/subset_vars.R")
 
 data_file_name <- "imputed-lasso-constructed.rds"
 prediction_name <- "lasso_lasso_imputation_constructed_covariates"
 
 # data ----
 train <- read_csv(file.path("data", "train.csv"))
-imputed_background <- readRDS(file.path("data", data_file_name))
+imputed_background <- readRDS(file.path("data", "imputed", data_file_name))
 
 # handle potential issues with imputed data
 # adds a challengeID column if necessary
@@ -52,8 +57,8 @@ alphas <- as.list(c(0.05, 0.10, 0.025, 0.15, 0.05, 0.05))
 x_cache <- lapply(outcomes, setup_x, data = ffc, covariates = covariates)
 x_pred_cache <- setup_x_pred(ffc, covariates)
 
-write_rds(x_cache, "data/x_cache_constructed.rds")
-write_rds(x_pred_cache, "data/x_pred_cache_constructed.rds")
+write_rds(x_cache, "data/cached/x_cache_constructed.rds")
+write_rds(x_pred_cache, "data/cached/x_pred_cache_constructed.rds")
 
 prediction_list <- 
   Map(f = function(...) { 
