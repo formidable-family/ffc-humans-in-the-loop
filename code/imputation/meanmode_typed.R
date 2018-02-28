39b93e61c4ea9de14c98997fb78b61d8402fbabc
Connor Gilroy <ccgilroy@gmail.com>
2018-02-27 17:01:34 -0800

    mean-mode (central tendency) imputation, using variable types

---
+++ code/imputation/meanmode_typed.R
@@ -0,0 +1,44 @@
+set.seed(123)
+
+library(devtools)
+
+if (!"FFCRegressionImputation" %in% installed.packages()) {
+  devtools::install_github("annafil/FFCRegressionImputation")
+}
+
+library(FFCRegressionImputation)
+library(dplyr)
+library(readr)
+
+# FFCRegressionImputation only supports parallelization 
+# on unix-based systems
+if (.Platform$OS.type == "unix") {
+  reg_parallel <- 1
+} else {
+  reg_parallel <- 0
+}
+
+
+# read background data
+yourDF <- initImputation(data = "data/background.csv") 
+
+# read lists of continuous and categorical variables
+vars_continuous <- read_lines("data/variables/continuous.txt")
+vars_categorical <- read_lines("data/variables/categorical.txt")
+
+# create correlation matrix
+output_typed <- corMatrix(data = yourDF, 
+                          continuous = vars_continuous, 
+                          categorical = vars_categorical)
+
+# impute data frame
+meanmode_typed_df <- regImputation(yourDF, output_typed,  
+                                   method = "",
+                                   top_predictors = 0, 
+                                   threshold = 1, 
+                                   continuous = vars_continuous, 
+                                   categorical = vars_categorical,
+                                   parallel = reg_parallel, 
+                                   failmode = "impute")
+
+saveRDS(meanmode_typed_df, "data/imputed/meanmode_imputed.rds")
--- code/run_all.sh
+++ code/run_all.sh
@@ -8,10 +8,12 @@ Rscript code/imputation/setup_mi_data.R &&
 Rscript code/imputation/mi.R &&
 Rscript code/imputation/mi_constructed.R &&
 Rscript code/imputation/reg_lm_typed.R &&
+Rscript code/imputation/meanmode_typed.R &&
 
 # run models
 Rscript code/runs/run_lasso_mi.R &&
 Rscript code/runs/run_lasso_mi_constructed.R &&
 Rscript code/runs/run_lasso_all.R &&
+Rscript code/runs/run_lasso_all_mean.R &&
 
 echo "Done"
