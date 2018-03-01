24a7e9bd684bb1261a52453a0831db8b9ae3bd54
Connor Gilroy <ccgilroy@gmail.com>
2018-02-28 19:07:46 -0800

    add imputation based on regularized regression. this allows running the main run_lasso script

---
+++ code/imputation/reg_lasso_untyped.R
@@ -0,0 +1,36 @@
+set.seed(123)
+
+library(devtools)
+
+if (!"FFCRegressionImputation" %in% installed.packages()) {
+  devtools::install_github("annafil/FFCRegressionImputation")
+}
+
+library(FFCRegressionImputation)
+library(dplyr)
+library(readr)
+
+# FFCRegressionImputation only supports parallelization 
+# on unix-based systems
+if (.Platform$OS.type == "unix") {
+  reg_parallel <- 1
+} else {
+  reg_parallel <- 0
+}
+
+
+# read background data
+yourDF <- initImputation(data = "data/background.csv") 
+
+# create correlation matrix
+output_untyped <- corMatrix(data=yourDF)
+
+# impute data frame
+lasso_untyped_df <- regImputation(yourDF, output_typed,    
+                                  method = "lasso",
+                                  top_predictors = 5, 
+                                  threshold = .1, 
+                                  parallel = reg_parallel, 
+                                  failmode = "impute")
+
+saveRDS(meanmode_typed_df, "data/imputed/imputed-fulldata-lasso.rds")
--- code/run_all.sh
+++ code/run_all.sh
@@ -9,10 +9,12 @@ Rscript code/imputation/mi.R &&
 Rscript code/imputation/mi_constructed.R &&
 Rscript code/imputation/reg_lm_typed.R &&
 Rscript code/imputation/meanmode_typed.R &&
+Rscript code/imputation/reg_lasso_untyped.R &&
 
 # run models
 Rscript code/runs/run_lasso_mi.R &&
 Rscript code/runs/run_lasso_mi_constructed.R &&
+Rscript code/runs/run_lasso.R &&
 Rscript code/runs/run_lasso_all.R &&
 Rscript code/runs/run_lasso_all_mean.R &&
 
