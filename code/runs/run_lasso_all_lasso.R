ee6f1925cdf25e589a6e25a02daa49fcee9542ca
Connor Gilroy <ccgilroy@gmail.com>
2018-03-07 14:17:08 -0800

    cut lasso + all covariates + scores, because it runs too slowly for unclear reasons and fails to complete

--- code/runs/run_lasso_all_lasso.R
+++ code/runs/run_lasso_all_lasso.R
@@ -53,46 +53,6 @@ families <- as.list(c(rep("gaussian", 3),
 
 alphas <- as.list(c(0.05, 0.10, 0.025, 0.15, 0.05, 0.05))
 
-# scores ----
-ffvars_scored <-
-  read_csv(file.path("data", "variables", "ffvars_scored.csv")) %>%
-  filter(!is.na(ffvar))
-
-# handle duplicates by taking max score
-ffvars_scored <-
-  ffvars_scored %>%
-  group_by(outcome, ffvar) %>%
-  summarise(experts = max(experts, na.rm=TRUE),
-            mturkers = max(mturkers, na.rm=TRUE)) %>%
-  mutate(experts = ifelse(is.infinite(experts), NA, experts),
-         mturkers = ifelse(is.infinite(mturkers), NA, mturkers))
-
-all_covariates <- rep(list(colnames(imputed_background)[-1]), 6)
-names(all_covariates) <- c("gpa", "grit", "material_hardship",
-                           "eviction", "layoff", "job_training")
-covariates_df <-
-  all_covariates %>%
-  as_data_frame() %>%
-  gather("outcome", "ffvar")
-
-ffvars_scored <-
-  covariates_df %>%
-  left_join(ffvars_scored, by = c("outcome", "ffvar"))
-
-gpa_vars <- ffvars_scored %>% filter(outcome == "gpa")
-grit_vars <- ffvars_scored %>% filter(outcome == "grit")
-materialHardship_vars <- ffvars_scored %>% filter(outcome == "material_hardship")
-eviction_vars <- ffvars_scored %>% filter(outcome == "eviction")
-layoff_vars <- ffvars_scored %>% filter(outcome == "layoff")
-jobTraining_vars <- ffvars_scored %>% filter(outcome == "job_training")
-
-vars_data_list <- list(gpa_vars, grit_vars, materialHardship_vars,
-                       eviction_vars, layoff_vars, jobTraining_vars)
-names(vars_data_list) <- as.character(outcomes)
-
-scores_experts <- map(vars_data_list, "experts")
-scores_mturkers <- map(vars_data_list, "mturkers")
-
 # set up covariates ----
 # these steps are time consuming!
 # approximately 1hr each
@@ -115,34 +75,6 @@ prediction_list <-
   x_cache = x_cache,
   alpha = alphas)
 
-# with expert score information
-prediction_list_experts <-
-  Map(f = function(...) {
-    lasso(data = ffc,
-          covariates = covariates,
-          x_pred_cache = x_pred_cache,
-          ..., parallel = TRUE)$pred
-  },
-  outcome = outcomes,
-  scores = scores_experts,
-  family = families,
-  x_cache = x_cache,
-  alpha = alphas)
-
-# with mturk score information
-prediction_list_mturkers <-
-  Map(f = function(...) {
-    lasso(data = ffc,
-          covariates = covariates,
-          x_pred_cache = x_pred_cache,
-          ..., parallel = TRUE)$pred
-  },
-  outcome = outcomes,
-  scores = scores_mturkers,
-  family = families,
-  x_cache = x_cache,
-  alpha = alphas)
-
 # predictions ----
 names(prediction_list) <- as.character(outcomes)
 prediction <-
@@ -150,23 +82,6 @@ prediction <-
   select(challengeID) %>%
   bind_cols(prediction_list)
 
-names(prediction_list_experts) <- as.character(outcomes)
-prediction_experts <-
-  ffc %>%
-  select(challengeID) %>%
-  bind_cols(prediction_list_experts)
-
-names(prediction_list_mturkers) <- as.character(outcomes)
-prediction_mturkers <-
-  ffc %>%
-  select(challengeID) %>%
-  bind_cols(prediction_list_mturkers)
-
 # output ----
 # write to csv and zip for submission
-prediction_name_experts <- paste0(prediction_name, "_experts")
-prediction_name_mturkers <- paste0(prediction_name, "_mturkers")
-
 zip_prediction(prediction, prediction_name, run_file = "run_lasso_all_lasso.R")
-zip_prediction(prediction_experts, prediction_name_experts, run_file = "run_lasso_all_lasso.R")
-zip_prediction(prediction_mturkers, prediction_name_mturkers, run_file = "run_lasso_all_lasso.R")
