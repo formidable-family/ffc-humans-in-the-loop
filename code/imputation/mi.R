7296ac9af1fa7580d59560b515dd96bb0725d079
Connor Gilroy <ccgilroy@gmail.com>
2018-03-07 20:30:27 -0800

    change parallelization method for amelia so that each process uses a specific random seed

--- code/imputation/mi.R
+++ code/imputation/mi.R
@@ -2,16 +2,12 @@ set.seed(123)
 
 library(Amelia)
 library(parallel)
+library(foreach)
+library(doParallel)
 library(labelled)
-detectCores(logical = FALSE)
 
-# the parallel backend to use for Amelia is different 
-# between windows and unix-based systems
-if (.Platform$OS.type == "windows") {
-  options(amelia.parallel = "snow")
-} else {
-  options(amelia.parallel = "multicore")
-}
+detectCores(logical = FALSE)
+registerDoParallel(cores = 5)
 
 source("code/data_processing/R/get_vars.R")
 
@@ -20,14 +16,22 @@ background_ffvars <- readRDS("data/imputed/mi_setup/background_ffvars_to_mi.rds"
 categorical <- get_vars_categorical(background_ffvars)
 
 Sys.time()
+
+# https://lists.gking.harvard.edu/pipermail/amelia/2013-June/001023.html
+# https://gist.github.com/jrnold/1236095/4f6076e824bbefec32f03c446f6cefb70f651a2e
 mi_background_ffvars <- 
-  amelia(background_ffvars, m = 5, 
-         idvars = c("challengeID"),
-         noms = categorical, 
-         empri = .1 * nrow(background_ffvars), 
-         tolerance = .005,
-         boot.type = "none", 
-         ncpus = 5)
+  foreach(i = 1:5, 
+          .combine = "ameliabind", 
+          .packages = "Amelia") %dopar% {
+            set.seed(i)
+            amelia(background_ffvars, m = 1, 
+                   idvars = c("challengeID"),
+                   noms = categorical, 
+                   empri = .1 * nrow(background_ffvars), 
+                   parallel = "no",
+                   tolerance = .005,
+                   boot.type = "none")
+          }
 
 saveRDS(mi_background_ffvars, "data/imputed/background_ffvars_amelia.rds")
 Sys.time()
--- code/imputation/mi_constructed.R
+++ code/imputation/mi_constructed.R
@@ -2,16 +2,12 @@ set.seed(123)
 
 library(Amelia)
 library(parallel)
+library(foreach)
+library(doParallel)
 library(labelled)
-detectCores(logical = FALSE)
 
-# the parallel backend to use for Amelia is different 
-# between windows and unix-based systems
-if (.Platform$OS.type == "windows") {
-  options(amelia.parallel = "snow")
-} else {
-  options(amelia.parallel = "multicore")
-}
+detectCores(logical = FALSE)
+registerDoParallel(cores = 5)
 
 source("code/data_processing/R/get_vars.R")
 
@@ -22,14 +18,22 @@ background_constructed <- readRDS("data/imputed/mi_setup/background_constructed_
 categorical <- get_vars_categorical(background_constructed)
 
 Sys.time()
+
+# https://lists.gking.harvard.edu/pipermail/amelia/2013-June/001023.html
+# https://gist.github.com/jrnold/1236095/4f6076e824bbefec32f03c446f6cefb70f651a2e
 mi_background_constructed <- 
-  amelia(background_constructed, m = 5, 
-         idvars = c("challengeID"),
-         noms = categorical, 
-         empri = .1 * nrow(background_constructed), 
-         tolerance = .005,
-         boot.type = "none", 
-         ncpus = 5)
+  foreach(i = 1:5, 
+          .combine = "ameliabind", 
+          .packages = "Amelia") %dopar% {
+            set.seed(i)
+            amelia(background_constructed, m = 1, 
+                   idvars = c("challengeID"),
+                   noms = categorical, 
+                   empri = .1 * nrow(background_constructed), 
+                   parallel = "no",
+                   tolerance = .005,
+                   boot.type = "none")
+          }
 
 saveRDS(mi_background_constructed, "data/imputed/background_constructed_amelia.rds")
 Sys.time()
