2a4c4732f40dcc7c7479af933241843bc292ae99
Allison Morgan <allison.morgan@gmail.com>
2018-03-23 18:30:13 -0600

    Add summary statistics to plot_missingness

--- output/figures/plot_missingness.R
+++ output/figures/plot_missingness.R
@@ -16,9 +16,25 @@ x <- gg_miss_case(background)
 
 gg_miss_case(background, order_cases = TRUE)
 
+x <- gg_miss_case(background)
+summary_row <- miss_case_summary(background)
+print(summary_row)
+avg_row <- mean(as.numeric(summary_row$pct_miss))
+print(sprintf("Average %% missing within a row %.4f", avg_row))
+
+
 y <- 
   gg_miss_var(background, show_pct = TRUE) + 
   theme(axis.ticks.y=element_blank(), axis.text.y=element_blank())
 
 gg_miss_var(background, show_pct = TRUE) + 
   theme(axis.ticks.y=element_blank(), axis.text.y=element_blank())
+
+summary_col <- miss_var_summary(background)
+print(summary_col)
+avg_col <- mean(as.numeric(summary_col$pct_miss))
+# print(sprintf("Average %% missing within a column %.4f", avg_col)) # Should be the same as above
+
+d <- dim(background)
+total_miss <- sum(as.numeric(summary_col$n_miss))*100/(d[1]*d[2])
+print(sprintf("%% missing within the whole table %.4f", total_miss))
